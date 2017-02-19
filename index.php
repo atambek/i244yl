605676d8b7bca628bca0ef58b9dba2459f74df5e
Andres Tambek <ant@columbusglobal.com>
2017-02-19 19:34:24 +0200

    playing with text

--- index.php
+++ index.php
@@ -5,11 +5,57 @@
     <title>Andres Tambeki kodukas</title>
     <link rel="stylesheet" type="text/css" href="mystyle.css">
   </head>
+
   <body>
-    See on Andres Tambeku üks esimesi veebilehti. Minu Linkedin profiil asub 
-    <a href="https://linkedin.com/in/andres-tambek-425b533b">siin</a><br>
-    
-	<img src="pics/ant.jpeg" alt="Portreepilt" width="512" height="384" >
-	<?php echo phpversion('tidy') ?>
+    <h1>
+	Tere tulemast minu kodukale!
+	</h1>
+    <h2>
+	Alapealkiri 2
+	</h2>
+    <h3>
+	Alapealkiri 3
+	</h3>	
+    <h4>
+	Alapealkiri 4
+	</h4>	
+    <h5>
+	Alapealkiri 5
+	</h5>	
+    <h6>
+	Alapealkiri 6
+	</h6>		
+	<p>
+		See on Andres Tambeku üks esimesi veebilehti. <br>
+		<img src="pics/ant.jpeg" alt="Portreepilt" width="512" height="384" >
+	</p>
+	<p>
+		Tere! Andres nimeks. Üks haridus juba TÜ majandusteaduskonnast all.
+		Õppetraumadest ülesaamine võttis pealt 10 aastat aega ja siin ma nüüd siis taas omadega olen.
+		Elu viis juba tegelikult üsna varakult majandusvaldkonnast pigem IT teemadesse, kuigi tööl on need siiski tugevalt läbipõimunud.
+		Minu ainus ja seni viimane töökoht on olnud Columbus Eestis majandustarkvara konsultandi/arendajana.
+		Tegutsenud selles valdkonnas nüüdseks üle 11 aasta.
+		
+		<ul>
+			<li>element1</li>
+			<li>element2</li>
+			<li>element3</li>
+		</ul>
+		<ol>
+			<li>element1</li>
+			<li>element2</li>
+			<li>element3</li>
+		</ol>
+	</p>
+	<p>
+	Minu Linkedin profiil asub 
+		<a href="https://linkedin.com/in/andres-tambek-425b533b">siin</a>
+	</p>
+	<p>
+		<a href="http://validator.w3.org/check?uri=referer">
+			<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+		</a>
+	</p>
+	<?php echo phpversion("tidy")?>
   </body>
 </html>
