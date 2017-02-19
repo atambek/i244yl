7aa1c3464013224eb35a6dcc499e11a2b276dfda
Andres Tambek <ant@columbusglobal.com>
2017-02-19 18:04:32 +0200

    first i244 commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Andres Tambeki kodukas</title>
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+  </head>
+  <body>
+    See on Andres Tambeku üks esimesi veebilehti. Minu Linkedin profiil asub 
+    <a href="https://linkedin.com/in/andres-tambek-425b533b">siin</a><br>
+    
+	<img src="pics/ant.jpeg" alt="Portreepilt" width="512" height="384" >
+	<?php echo phpversion('tidy') ?>
+  </body>
+</html>
